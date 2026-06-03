cd826a478eee1ecd989ec9dd465c03937ab9097a
Coding <vatigyan809@gmail.com>
2026-06-03 09:37:17 +0530

    Day 7

---
+++ Day 7/Q27.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int sum(int n)
+{
+    if(n==0)
+        return 0;
+
+    return n%10 + sum(n/10);
+}
+
+int main()
+{
+    int n;
+    printf("Enter number: ");
+    scanf("%d",&n);
+
+    printf("Sum = %d", sum(n));
+
+    return 0;
+}
