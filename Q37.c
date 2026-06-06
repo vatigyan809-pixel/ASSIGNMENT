09e582c0cae6b6155c02a2a0595055164d73f12e
Raunak Verma <vatigyan809@gmail.com>
2026-06-06 11:40:32 +0530

    Add files via upload

---
+++ Q37.c
@@ -0,0 +1,34 @@
+<<<<<<< HEAD
+
+=======
+>>>>>>> 4930ad2 (Day 10)
+#include<stdio.h>
+int main (){
+    int i,j,k;
+    for(i=1;i<=5;i++)
+    {
+<<<<<<< HEAD
+        for(j=1;j<=5-i;j++)
+=======
+        for(j=4;j>=i;j--)
+>>>>>>> 4930ad2 (Day 10)
+        {
+            printf(" ");
+
+        }
+        for(k=1;k<=(2*i)-1;k++){
+<<<<<<< HEAD
+         printf("*");
+        }
+       
+=======
+        printf("*");
+        }
+        
+>>>>>>> 4930ad2 (Day 10)
+        printf("\n");
+    }
+    return 0;
+    
+
+}
---
+++ Q40.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main (){
+    int i,j,k;
+    for(i=65;i<=69;i++)
+    {
+        for(j=69;j>i;j--)
+        {
+            printf(" ");
+
+        }
+        for(k=65;k<=i;k++){
+         printf("%c",k);
+        }
+        for(k=i-1;k>=65;k--)
+        {
+           printf("%c",k);
+        }
+        
+        printf("\n");
+    }
+    return 0;
+    
+
+}
