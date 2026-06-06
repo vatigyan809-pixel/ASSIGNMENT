33b15b8365b9a22f74d1c4a6d92cb26b3998bdaa
Coding <vatigyan809@gmail.com>
2026-06-06 11:31:03 +0530

    Day 10

--- Day 10/Q37.c
+++ Day 10/Q37.c
@@ -1,17 +1,18 @@
+
 #include<stdio.h>
 int main (){
     int i,j,k;
     for(i=1;i<=5;i++)
     {
-        for(j=4;j>=i;j--)
+        for(j=1;j<=5-i;j++)
         {
             printf(" ");
 
         }
         for(k=1;k<=(2*i)-1;k++){
-        printf("*");
+         printf("*");
         }
-        
+       
         printf("\n");
     }
     return 0;
